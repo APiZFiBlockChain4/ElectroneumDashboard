e5c082ae371da86b5939ecf7795c26b190d6d889
liejoes1 <liejoes1@gmail.com>
2018-02-10 23:41:32 +0800

    Code is cleaned completely into function

---
+++ app/src/main/java/com/example/joes/electroneumwallet/JSONUtil.java
@@ -0,0 +1,57 @@
+package com.example.joes.electroneumwallet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Joes on 10/2/2018.
+ */
+
+class JSONUtil {
+     static String ETN_to_USD = "";
+     static String ETN_to_BTC = "";
+     static String ETN_Price_Change = "";
+     static String Mine_HashRate = "";
+     static String Mine_Balance = "";
+     static String Mine_LastUpdate = "";
+
+     static void GetMiningData(String FullJsonResponse) {
+        try {
+            JSONObject BaseJsonResponse = new JSONObject(FullJsonResponse);
+            JSONObject StatsJson = BaseJsonResponse.getJSONObject("stats");
+            if (StatsJson.has("hashrate")) {
+                Mine_HashRate = StatsJson.getString("hashrate");
+                Mine_HashRate = Mine_HashRate.substring(0, Mine_HashRate.length() - 1);
+            }
+            else {
+                Mine_HashRate = "0";
+            }
+
+
+
+            Mine_Balance = StatsJson.getString("balance");
+            Double TempBalance = Double.parseDouble(Mine_Balance)/100;
+            Mine_Balance = TempBalance + " ETN";
+
+            Mine_LastUpdate = StatsJson.getString("lastShare");
+            long LastUpdateMinute = (System.currentTimeMillis()/1000 - Long.parseLong(Mine_LastUpdate))/60;
+            Mine_LastUpdate = "Last Update " + LastUpdateMinute + " Minute";
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+     static void GetCurrencyData(String FullJsonResponse) throws JSONException {
+        JSONArray BaseJsonResponse = new JSONArray(FullJsonResponse);
+        JSONObject FirstObject = BaseJsonResponse.getJSONObject(0);
+        ETN_to_USD = FirstObject.getString("price_usd");
+        ETN_to_BTC = FirstObject.getString("price_btc");
+        ETN_Price_Change = FirstObject.getString("percent_change_1h") + " %";
+    }
+
+
+}
--- app/src/main/java/com/example/joes/electroneumwallet/MainActivity.java
+++ app/src/main/java/com/example/joes/electroneumwallet/MainActivity.java
@@ -1,25 +1,22 @@
 package com.example.joes.electroneumwallet;
 
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import java.io.IOException;
+import android.widget.TextView;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    public static TextView Current_ETN_Price_USD_TextView;
-    public static TextView Current_ETN_Price_BTC_TextView;
-    public static TextView Current_ETN_Price_Change;
-    public static TextView Current_ETN_MineSpeed;
-    public static TextView Current_ETN_Balance;
-    public static TextView Current_ETN_TimeDif;
-    public static TextView Unpaid_USD_Balance;
-    public static TextView Unpaid_BTC_Balance;
+    private static TextView Current_ETN_Price_USD_TextView;
+    private static TextView Current_ETN_Price_BTC_TextView;
+    private static TextView Current_ETN_Price_Change;
+    private static TextView Current_ETN_MineSpeed;
+    private static TextView Current_ETN_Balance;
+    private static TextView Current_ETN_TimeDif;
+    private static TextView Unpaid_USD_Balance;
+    private static TextView Unpaid_BTC_Balance;
 
 
 
@@ -27,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Current_ETN_Price_USD_TextView = (TextView) findViewById(R.id.tv_current_price_usd);
-        Current_ETN_Price_BTC_TextView = (TextView) findViewById(R.id.tv_current_price_btc);
-        Current_ETN_Price_Change = (TextView) findViewById(R.id.tv_current_price_changes);
-        Current_ETN_MineSpeed = (TextView) findViewById(R.id.tv_unpaid_speed);
-        Current_ETN_Balance = (TextView) findViewById(R.id.tv_unpaid_balance_etn);
-        Current_ETN_TimeDif = (TextView) findViewById(R.id.tv_unpaid_balance_time);
-        Unpaid_USD_Balance = (TextView) findViewById(R.id.tv_unpaid_balance_usd);
-        Unpaid_BTC_Balance = (TextView) findViewById(R.id.tv_unpaid_balance_btc);
+        Current_ETN_Price_USD_TextView = findViewById(R.id.tv_current_price_usd);
+        Current_ETN_Price_BTC_TextView = findViewById(R.id.tv_current_price_btc);
+        Current_ETN_Price_Change = findViewById(R.id.tv_current_price_changes);
+        Current_ETN_MineSpeed = findViewById(R.id.tv_unpaid_speed);
+        Current_ETN_Balance = findViewById(R.id.tv_unpaid_balance_etn);
+        Current_ETN_TimeDif = findViewById(R.id.tv_unpaid_balance_time);
+        Unpaid_USD_Balance = findViewById(R.id.tv_unpaid_balance_usd);
+        Unpaid_BTC_Balance = findViewById(R.id.tv_unpaid_balance_btc);
 
 
         try {
@@ -42,6 +39,33 @@ public class MainActivity extends AppCompatActivity {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+    }
+
+     public static void UpdateUIWithData() {
+        Current_ETN_Price_USD_TextView.setText(JSONUtil.ETN_to_USD);
+        Current_ETN_Price_BTC_TextView.setText(JSONUtil.ETN_to_BTC);
+        Current_ETN_Price_Change.setText(JSONUtil.ETN_Price_Change);
+        Current_ETN_MineSpeed.setText(JSONUtil.Mine_HashRate);
+        Current_ETN_Balance.setText(JSONUtil.Mine_Balance);
+        Current_ETN_TimeDif.setText(JSONUtil.Mine_LastUpdate);
+        Double Mine_Balance_Float = Double.parseDouble(JSONUtil.Mine_Balance.substring(0, JSONUtil.Mine_Balance.length()-4));
+        Double ETN_to_USD_Float = Double.parseDouble(JSONUtil.ETN_to_USD) * Mine_Balance_Float;
+        Double ETN_to_BTC_Float = Double.parseDouble(JSONUtil.ETN_to_BTC) * Mine_Balance_Float;
+        DecimalFormat df = new DecimalFormat("#");
+        df.setMaximumFractionDigits(8);
+        Unpaid_USD_Balance.setText(String.valueOf(ETN_to_USD_Float) + " USD");
+        Unpaid_BTC_Balance.setText("0" + String.valueOf(df.format( ETN_to_BTC_Float)) + " BTC");
+    }
+
+    public static void CleanUIData() {
+        Current_ETN_Price_USD_TextView.setText("");
+        Current_ETN_Price_BTC_TextView.setText("");
+        Current_ETN_Price_Change.setText("");
+        Current_ETN_MineSpeed.setText("");
+        Current_ETN_Balance.setText("");
+        Current_ETN_TimeDif.setText("");
+        Unpaid_USD_Balance.setText("");
+        Unpaid_BTC_Balance.setText("");
     }
 
 
